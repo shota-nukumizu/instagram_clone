3589ff6fe71bf2e420d79675051d8708feeb5c09
shota-nukumizu <author2000.1225@gmail.com>
2022-07-17 15:25:07 +0900

    add assets

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -7,7 +7,8 @@ class InstaHome extends StatelessWidget {
     centerTitle: true,
     elevation: 1.0,
     leading: new Icon(Icons.camera_alt),
-    title: SizedBox(height: 35.0, child: Image.asset("assets/insta_logo.png")),
+    title: SizedBox(
+        height: 35.0, child: Image(image: AssetImage('insta_logo.png'))),
     actions: <Widget>[
       Padding(
         padding: const EdgeInsets.only(right: 12.0),
